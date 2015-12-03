b64c2424f8944958eca74d1ab0232965ead15be5
Gunnar Bastkowski <github@digitalstep.de>
2015-12-03 01:04:40 +0100

    io should be allright for now

--- src/test/scala/de/digitalstep/timetrack/io/TextStorageSpecification.scala
+++ src/test/scala/de/digitalstep/timetrack/io/TextStorageSpecification.scala
@@ -9,13 +9,13 @@ class TextStorageSpecification extends PropertySpecification {
 
   import Generators.Implicits._
 
-  property("TextStorage.sections") {
+  property("sections") {
     forAll { data: InputText ⇒
       new TextStorage(() ⇒ data.sections, mockSerializer).sections should contain theSameElementsAs data.sections
     }
   }
 
-  property("TextStorage.findDay") {
+  property("findDay") {
     forAll { data: InputText ⇒
       data.sections foreach {
         case day@Day(date, _) ⇒ new TextStorage(() ⇒ data.sections, mockSerializer).findDay(date).map(_.date) should be(Some(date))
@@ -28,16 +28,18 @@ class TextStorageSpecification extends PropertySpecification {
   val testDate2 = date(2015, 12, 2)
   val testTask1 = Task(LocalTime.of(9, 0), LocalTime.of(10, 0), "Test")
   val testTask2 = Task(time(11, 0), time(12, 0), "Test")
+
   private val mockSerializer = new Serializer {
     val out = new ByteArrayOutputStream()
+
     protected def outputStream(): OutputStream = out
 
     override protected def close(out: AutoCloseable): Unit = out.close()
   }
 
-  val tested = new TextStorage(
+  def tested = new TextStorage(
     () ⇒ List(
-      Comment("Comment"),
+      Comment("-*-Text-*-"),
       Day(testDate1, Seq(testTask1)),
       Day(date(2015, 11, 30), Seq(testTask1, testTask2))),
     mockSerializer)
@@ -53,8 +55,17 @@ class TextStorageSpecification extends PropertySpecification {
 
   property("save") {
     tested.save()
-    val result = mockSerializer.out.toString("UTF-8")
-    println(s"Result:\n $result")
+
+    mockSerializer.out.toString("UTF-8") shouldBe
+      """# -*-Text-*-
+        |2015-12-01
+        |09:00 - 10:00   Test
+        |
+        |2015-11-30
+        |09:00 - 10:00   Test
+        |11:00 - 12:00   Test
+        |
+        |""".stripMargin
   }
 
 }
