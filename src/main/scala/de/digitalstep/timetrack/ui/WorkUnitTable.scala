13c4812badffc9febe30bfd4033150c835840012
Gunnar Bastkowski <github@digitalstep.de>
2015-12-16 23:07:16 +0100

    clean code

--- src/main/scala/de/digitalstep/timetrack/ui/WorkUnitTable.scala
+++ src/main/scala/de/digitalstep/timetrack/ui/WorkUnitTable.scala
@@ -1,5 +1,7 @@
 package de.digitalstep.timetrack.ui
 
+import java.time.{LocalTime, LocalDate}
+
 import scalafx.beans.value.ObservableValue
 import scalafx.collections.ObservableBuffer
 import scalafx.scene.control.{TableColumn, TableView}
@@ -10,39 +12,32 @@ class WorkUnitTable(buffer: ObservableBuffer[WorkUnitAdapter]) extends TableView
   type CellValueFactory[T] = TableColumn.CellDataFeatures[WorkUnitAdapter, String] => ObservableValue[T, T]
   type WorkUnitColumn = TableColumn[WorkUnitAdapter, String]
 
-  def column(_text: String, observable: CellValueFactory[String]): WorkUnitColumn = new WorkUnitColumn {
+  columns ++= Seq(
+    dateColumn("Date", _.value.dayProperty),
+    timeColumn("From", _.value.fromProperty),
+    timeColumn("To", _.value.toProperty),
+    stringColumn("Description", _.value.descriptionProperty)
+  )
+
+  private[this] def dateColumn(text: String, property: CellValueFactory[LocalDate]) =
+    column(text, property, LocalDateStringConverter.shortFormatStyle)
+
+  private[this] def timeColumn(text: String, property: CellValueFactory[LocalTime]) =
+    column(text, property, LocalTimeStringConverter.short)
+
+
+  private[this] def stringColumn(_text: String, observable: CellValueFactory[String]): WorkUnitColumn = new WorkUnitColumn {
     text = _text
     cellValueFactory = x ⇒ observable(x)
   }
 
-  def column[T](_text: String,
-                observable: CellValueFactory[T],
-                converter: StringConverter[T]): WorkUnitColumn = new WorkUnitColumn {
-    text = _text
-    cellValueFactory = x ⇒ StringBinding(observable(x), converter)
-  }
-
-  columns ++= Seq(
-    column(
-      "Date",
-      _.value.dayProperty,
-      LocalDateStringConverter.shortFormatStyle
-    ),
-    column(
-      "From",
-      _.value.fromProperty,
-      LocalTimeStringConverter.short
-    ),
-    column(
-      "To",
-      _.value.toProperty,
-      LocalTimeStringConverter.short
-    ),
-    column(
-      "Description",
-      _.value.descriptionProperty
-    )
-  )
+  private[this] def column[T](_text: String,
+                              observable: CellValueFactory[T],
+                              converter: StringConverter[T]): WorkUnitColumn =
+    new WorkUnitColumn {
+      text = _text
+      cellValueFactory = x ⇒ StringBinding(observable(x), converter)
+    }
 
   items = buffer
 }
