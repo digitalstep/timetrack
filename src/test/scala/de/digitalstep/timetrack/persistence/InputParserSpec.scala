77d98661e7d9402c1876ebdf020bf528eea419c5
Gunnar Bastkowski <github@digitalstep.de>
2015-12-20 21:50:36 +0100

    timetrack-2 Remove comment feature for now

--- src/test/scala/de/digitalstep/timetrack/persistence/InputParserSpec.scala
+++ src/test/scala/de/digitalstep/timetrack/persistence/InputParserSpec.scala
@@ -11,11 +11,6 @@ class InputParserSpec extends UnitSpec {
     new InputParser("#asdf").comment.run().get shouldEqual Comment("asdf")
   }
 
-  it should "be contained in a list of sections" in {
-    new InputParser("#asdf").all.run().get shouldEqual InputText(Seq(Comment("asdf")))
-  }
-
-
   import CompleteFiles._
 
   "A simple timesheet" should "be parsed" in {
@@ -29,24 +24,17 @@ class InputParserSpec extends UnitSpec {
     )
   }
 
-  it should "handle and store comments" in {
-    val days: InputText = new InputParser(withComment).all.run().get
-
-    days.sections.head shouldEqual Comment("This is a comment")
-  }
-
-  it should "handle and store comments in between other top level elements" in {
+  it should "handle multiple top level elements" in {
     val days: InputText = new InputParser(multipleSectionsWithComment).all.run().get
-    val s1 :: comment :: s2 :: Nil = days.sections.toList
+    val s1 :: s2 :: Nil = days.sections.toList
 
     s1 shouldEqual Day(
-      LocalDate.of(2015, 11, 16),
+      date(2015, 11, 16),
       Seq(
-        Task(Time(9, 45), Time(12, 0), "Coding"),
-        Task(Time(13, 0), Time(18, 0), "Coding2")
+        Task(time(9, 45), time(12, 0), "Coding"),
+        Task(time(13, 0), time(18, 0), "Coding2")
       )
     )
-    comment shouldEqual Comment(" This is another comment")
     s2 shouldEqual Day(
       LocalDate.of(2015, 11, 17),
       Seq(
@@ -61,14 +49,11 @@ object CompleteFiles {
 
   val simple = "2015-11-16\n 9:45 - 12:00   Coding\n\n"
 
-  val withComment = "#This is a comment"
-
   val multipleSectionsWithComment =
     """2015-11-16
       | 9:45 - 12:00   Coding
       |13:00 - 18:00   Coding2
       |
-      |# This is another comment
       |2015-11-17
       |10:00 - 11:00   Coding
       | """.stripMargin
