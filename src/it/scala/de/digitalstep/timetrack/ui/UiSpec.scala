8cd0516e509384007caefa739c5930aaa52eebad
Gunnar Bastkowski <github@digitalstep.de>
2018-09-02 22:19:28 +0200

    Create integration test

--- src/it/scala/de/digitalstep/timetrack/ui/UiSpec.scala
+++ src/it/scala/de/digitalstep/timetrack/ui/UiSpec.scala
@@ -1,13 +1,10 @@
 package de.digitalstep.timetrack.ui
 
-import java.util.function.Consumer
+import com.typesafe.scalalogging.LazyLogging
 import javafx.scene.Scene
 import javafx.stage.Stage
-
-import com.typesafe.scalalogging.LazyLogging
 import org.scalatest.{BeforeAndAfter, FlatSpec}
 import org.testfx.api.FxToolkit._
-
 import scalafx.Includes._
 import scalafx.scene.control.{Button, Label}
 import scalafx.scene.layout.BorderPane
@@ -18,16 +15,14 @@ class UiSpec extends FlatSpec with BeforeAndAfter with LazyLogging {
   showStage()
 
   before {
-    setupStage(new Consumer[Stage] {
-      override def accept(t: Stage): Unit = t.setScene(new Scene(new BorderPane {
-        val out = new Label("out")
-        val in = new Button("In") {
-          onAction = () ⇒ out.text = "Done"
-        }
-        center = out
-        bottom = in
-      }.delegate))
-    })
+    setupStage((t: Stage) => t.setScene(new Scene(new BorderPane {
+      val out = new Label("out")
+      val in = new Button("In") {
+        onAction = () ⇒ out.text = "Done"
+      }
+      center = out
+      bottom = in
+    }.delegate)))
     showStage()
   }
 
